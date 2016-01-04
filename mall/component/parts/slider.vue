7950d7807091d2b479563de847e35c733a3c5d65
zw111 <zw@dianjoy.com>
2016-01-04 23:01:24 +0800

    vendor

---
+++ mall/component/parts/slider.vue
@@ -0,0 +1,18 @@
+<template>
+  <link rel="stylesheet" href="mall/vendor/bootstrap-slider/slider.css">
+  <input type="text" value="" class="slider form-control" data-slider-min="0" data-slider-max="100" data-slider-step="1" data-slider-orientation="horizontal" data-slider-selection="before" data-slider-tooltip="show" data-slider-id="green">
+</template>
+
+<script>
+$(function () {
+  $('.slider').slider();
+});
+
+  export default {
+    data() {
+
+    },
+    components: {
+    }
+  }
+</script>
