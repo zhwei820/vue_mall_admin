dd54b3fc8e29edac6f1f9f1df03cfac41a5309a8
Your Name <you@example.com>
2016-01-04 16:19:23 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var webpack = require('webpack')
 var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var vue = require('vue-loader');
+
 
 module.exports = {
   entry: './mall/index.js',
@@ -19,9 +22,23 @@ module.exports = {
         exclude: /node_modules|vue\/src|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
       	loader: 'babel'
       },
-      { test: /\.css$/, loader: "style-loader!css-loader?root=./docs/" }
+      { test: /\.css$/, loader: "style-loader!css-loader?root=./mall/" }
+      ,
+      {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=100000&name=mall/assets/img/[hash:8].[name].[ext]'
+    },
+
     ]
   },
+  vue: {
+    loaders: {
+      less: ExtractTextPlugin.extract('css!less')
+    }
+  },
+  plugins: [
+     new ExtractTextPlugin('./app.css'), // 输出到 output path 下的 app.css 文件
+   ],
   babel: {
   presets: ['es2015'],
   plugins: ['transform-runtime']
