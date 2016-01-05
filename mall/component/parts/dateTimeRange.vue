5b60efc5eb9d07b0c4118167cbb257f816e7d396
Your Name <you@example.com>
2016-01-05 10:00:18 +0800

    ++

---
+++ mall/component/parts/dateTimeRange.vue
@@ -0,0 +1,39 @@
+<template>
+    <link rel="stylesheet" href="./mall/vendor/daterangepicker/daterangepicker-bs3.css">
+    <link rel="stylesheet" href="./mall/vendor/timepicker/bootstrap-timepicker.min.css">
+
+    
+    <div class="form-group">
+        <label>开始结束时间</label>
+        <div class="input-group">
+          <div class="input-group-addon">
+            <i class="glyphicon glyphicon-time"></i>
+          </div>
+          <input type="text" class="form-control pull-right" id="reservationtime">
+        </div>
+        <!-- /.input group -->
+      </div>
+
+
+</template>
+
+<script>
+
+      $(document).ready(function(){
+          $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'YYYY-MM-DD hh:mm:ss'});
+
+      })
+
+    export default {
+        data() {
+          return {
+
+          }
+        },
+        props : {
+            lablel : String
+        },
+        components: {
+        }
+    }
+</script>
